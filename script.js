ffa9d2dfca8c43a3545039b80df2005f0f046019
siriprydz <siriprydz@gmail.com>
2025-10-22 21:56:03 +0200

    add functions for menu icon change on click

--- script.js
+++ script.js
@@ -26,9 +26,32 @@ if (window.MENU_ANIMATION_MODE === ANIMATION.NONE) {
 function toggleMenu() {
   console.log("button clicked");
   revealMenu();
+  changeMenuIcon();
+  changeMenuIconBackgroundColor();
 }
 
 function revealMenu() {
-  let SelectUlMenuCssRule = document.styleSheets[2].cssRules[8].style;
-  let changeUlMenuPosition = SelectUlMenuCssRule.setProperty("left", "0px");
+  let UlMenuCssRule = document.styleSheets[2].cssRules[8].style;
+  let UlMenuPosition = UlMenuCssRule.setProperty("left", "0px");
+}
+
+function changeMenuIcon() {
+  let menuIcon = document.querySelector("i");
+  menuIcon.classList.remove("fas", "fa-bars");
+  const crossIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x" viewBox="3 2.9 16 16">
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/></svg>`;
+  let iconDiv = document.querySelector("i");
+  iconDiv.innerHTML = crossIcon;
+}
+
+function changeMenuIconBackgroundColor() {
+  let MenuIconCssRule = document.styleSheets[2].cssRules[6].style;
+  let MenuIconBackgroundcolor = MenuIconCssRule.setProperty(
+    "background-color",
+    "azure"
+  );
+  let MenuIconBorderRadius = MenuIconCssRule.setProperty(
+    "border-radius",
+    "10px"
+  );
 }
